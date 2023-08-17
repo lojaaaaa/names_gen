a9196949fe87ce926c9adf1c56045ce6241d03f1
Alexander <a.ayugov@zebrains.team>
2023-08-17 20:20:28 +0400

    create name genearator

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [newName, setNewName] = useState<string>('')
+
+  const randomNumber = (min: number, max: number): number =>{
+    return Math.floor(Math.random() * (max - min + 1));
+  }
+
+  function* nameGenerator (){
+    const abc = 'abcdefghijklmnopqrstuvwxyz'
+
+    while(true){
+      let name: string = ''
+      for(let i = 0; i < 6; i++){
+        name += abc[randomNumber(0, abc.length - 1)]
+      }
+      yield `${name}`
+    }
+
+  }
+
+  const onClickButton = () =>{
+    const name = nameGenerator().next().value 
+    setNewName(name!)
+  }
+
+
   return (
     <div className="wrapper">
       <main className="main">
         <div className="main__container container">
-          <input type="text" className="input" placeholder='Здесь будет ваше имя'/>
-          <button className="button">Сгенерировать имя</button>
+          <input value={newName} type="text" className="input" placeholder='Здесь будет ваше имя'/>
+          <button onClick={onClickButton} className="button">Сгенерировать имя</button>
         </div>
       </main>
     </div>
